4d7e1147925d6b2b3bfd86f98f1c1d19e0292c2c
Sangjin <sl17@sanger.ac.uk>
2021-05-19 10:47:57 +0100

    fk

--- src/fastx/index.py
+++ src/fastx/index.py
@@ -1,21 +1,24 @@
 import os
 import pyfastx
 
+
 def build_index(infile):
     fxifile = infile + ".fxi"
     if os.path.exists(fxifile):
-        print("fxi index is present"):
+        print("fxi index is present")
     else:
         print("buliding fxi index for {}".format(infile))
         if infile.endswith((".fa", ".fa.gz", ".fasta", ".fasta.gz")):
             pyfastx.Fasta(infile)
-        else: 
+        else:
             pyfastx.Fastq(infile)
         print("fxi index has been created for {}".format(infile))
 
 
 def seq_index(infile):
-    if infile.endswith((".fa", ".fa.gz", ".fasta", ".fasta.gz", ".fq", ".fq.gz", ".fastq", ".fastq.gz")):
+    if infile.endswith(
+        (".fa", ".fa.gz", ".fasta", ".fasta.gz", ".fq", ".fq.gz", ".fastq", ".fastq.gz")
+    ):
         build_index(infile)
     else:
         print("fastx index doesn't support the provided input file")
